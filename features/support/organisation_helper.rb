18361da27eeffcd39961b1186e7e9a7cb83746e6
Edd Sowden <e@e26.co.uk>
2015-06-23 10:43:06 +0000

    Adding featured policies to org homepages
    Organisations want to be able to pick the policies they feature on their
    homepages. They also would like to be able to order the policies they
    feature on their homepage. This is the first pass at adding featured
    policies to organisations.
    
    It pulls in the policies from the content store and saves references to
    them so they can be displayed with their current name and slug on an org
    homepage.

--- features/support/organisation_helper.rb
+++ features/support/organisation_helper.rb
@@ -11,6 +11,19 @@ module OrganisationHelper
     fill_in "Logo formatted name", with: translation["logo formatted name"]
     click_on "Save"
   end
+
+  def feature_policies_on_organisation(policies)
+    click_link "Featured policies"
+    policies.each do |policy|
+      click_button "Feature #{policy}"
+    end
+  end
+
+  def unfeature_organisation_policy(policy)
+    click_link "Featured policies"
+    click_link "Unfeature #{policy}"
+  end
+
 end
 
 World(OrganisationHelper)
